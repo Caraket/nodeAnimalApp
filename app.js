77253d2320929054342212e714adc61e5a5a7cb8
Cory <Cory.Paulley@gmail.com>
2020-07-22 15:03:40 -0500

    moved routes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const express          = require("express"),
       session          = require("express-session"),
       app              = express();
 
+const routes = require("./routes");
+
 
 
 
@@ -32,85 +34,6 @@ const animalSchema = new mongoose.Schema({
     body: String,
 });
 const Animal = mongoose.model("Animal", animalSchema);
-
-// RESTful ROUTES
-
-//INDEX ROUTE
-app.get("/", (req, res) =>{
-    Animal.find({}, (err, animals) => {
-        if(err){
-            console.log(err);
-        } else{
-            res.render("index", {animals: animals});
-            
-        }
-    });
-});
-
-//NEW ROUTE
-app.get("/new", (req, res) => {
-    res.render("new");
-});
-
-//CREATE ROUTE
-app.post("/", (req, res) => {
-    Animal.create(req.body.animal, (err, newAnimal) => {
-        if(err){
-            res.render("new");
-        } else{
-            res.redirect("/");
-        }
-    });
-});
-
-app.get("/login", (req, res) => {
-    res.render("login");
-});
-
-//SHOW ROUTE
-app.get("/:id", (req, res) => {
-    Animal.findById(req.params.id, (err, foundAnimal) => {
-        if(err){
-            console.log(err);
-            res.redirect("/");
-        } else{
-            res.render("show", {animal: foundAnimal});
-        }
-    });
-});
-
-// EDIT ROUTE
-app.get("/:id/edit", (req, res) => {
-    Animal.findById(req.params.id, (err, foundAnimal) => {
-        if(err){
-            res.redirect("/");
-        } else{
-            res.render("edit", {animal: foundAnimal});
-        }
-    });
-});
-
-//UPDATE ROUTE
-app.put("/:id", (req, res) => {
-    Animal.findByIdAndUpdate(req.params.id, req.body.animal, (err, updatedAnimal) => {
-        if(err){
-            res.redirect("/");
-        } else{
-            res.redirect("/" + req.params.id);
-        }
-    })
-})
-
-//Delete Route
-app.delete("/:id/", (req, res) => {
-    Animal.findByIdAndDelete(req.params.id, (err, deletedAnimal) => {
-        if(err){
-            res.redirect("/");
-        } else{
-            res.redirect("/");
-        }
-    })
-});
 
 app.listen(3000, () => {
     console.log("App is listening on port 3000");
---
+++ routes.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const router = express.router;
+
+
+// RESTful ROUTES
+
+//INDEX ROUTE
+router.get("/", (req, res) =>{
+    Animal.find({}, (err, animals) => {
+        if(err){
+            console.log(err);
+        } else{
+            res.render("index", {animals: animals});
+            
+        }
+    });
+});
+
+//NEW ROUTE
+router.get("/new", (req, res) => {
+    res.render("new");
+});
+
+//CREATE ROUTE
+router.post("/", (req, res) => {
+    Animal.create(req.body.animal, (err, newAnimal) => {
+        if(err){
+            res.render("new");
+        } else{
+            res.redirect("/");
+        }
+    });
+});
+
+router.get("/login", (req, res) => {
+    res.render("login");
+});
+
+//SHOW ROUTE
+router.get("/:id", (req, res) => {
+    Animal.findById(req.params.id, (err, foundAnimal) => {
+        if(err){
+            console.log(err);
+            res.redirect("/");
+        } else{
+            res.render("show", {animal: foundAnimal});
+        }
+    });
+});
+
+// EDIT ROUTE
+router.get("/:id/edit", (req, res) => {
+    Animal.findById(req.params.id, (err, foundAnimal) => {
+        if(err){
+            res.redirect("/");
+        } else{
+            res.render("edit", {animal: foundAnimal});
+        }
+    });
+});
+
+//UPDATE ROUTE
+router.put("/:id", (req, res) => {
+    Animal.findByIdAndUpdate(req.params.id, req.body.animal, (err, updatedAnimal) => {
+        if(err){
+            res.redirect("/");
+        } else{
+            res.redirect("/" + req.params.id);
+        }
+    })
+})
+
+//Delete Route
+router.delete("/:id/", (req, res) => {
+    Animal.findByIdAndDelete(req.params.id, (err, deletedAnimal) => {
+        if(err){
+            res.redirect("/");
+        } else{
+            res.redirect("/");
+        }
+    })
+});
