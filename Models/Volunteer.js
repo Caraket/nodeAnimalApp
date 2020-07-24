7da4be4777a8de1923c11c65a2874a55b8bae612
Cory <cory.paulley@gmail.com>
2020-07-23 20:43:44 -0500

    fixed the volunteer model

--- Models/Volunteer.js
+++ Models/Volunteer.js
@@ -1,14 +1,12 @@
 var mongoose = require("mongoose");
 
 const volunteerSchema = new mongoose.Schema({
-    petId: String,
+    volunteerId: String,
     name: String,
-    sex: String,
-    breed: String,
-    age: {type: Number},
-    altered: String,
-    alteredDate: {type: Date},
-    intake: {type: Date, default: Date.now},
+    phoneNumber: String,
+    email: String,
+    address: String,
+    start: {type: Date, default: Date.now},
     created: {type: Date, default: Date.now},
     body: String,
 });
