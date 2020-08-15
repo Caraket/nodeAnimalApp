9c64e9e1ac61f396b2d5506035351e71203b97b8
Cory <cory.paulley@gmail.com>
2020-08-15 00:07:08 -0500

    Potential bug fix for volunteer show route

--- Routes/volunteerRoutes.js
+++ Routes/volunteerRoutes.js
@@ -42,7 +42,7 @@ router.get("/:id", (req, res) => {
             res.redirect("/volunteer");
         } else{
             const { userContext } = req;
-            res.render("./volunteer/show", {
+            res.render("/volunteer/show", {
                 volunteer: foundVolunteer,
                 userContext
             });
